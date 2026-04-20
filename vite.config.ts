63ed6dcb397b952918de9fe34774e00dc2697711
Asahi Tokunaga <122292089+AsahiTokunaga@users.noreply.github.com>
2026-04-20 11:50:42 +0000

    test commit for automatic release

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import electron from "vite-plugin-electron";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    electron([
+      {
+        entry: "electron/main.ts",
+      },
+      {
+        entry: path.resolve(__dirname, "electron/preload.ts"),
+      },
+    ]),
+  ],
+});
